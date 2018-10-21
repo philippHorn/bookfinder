665ae5ef0e7fde2b0d55b94d3dcd1fb827c89095
Philipp Horn <horn.philipp95@gmail.com>
2018-10-21 17:27:17 +0200

    Initial commit

---
+++ ebay.py
@@ -0,0 +1,61 @@
+from datetime import datetime, timedelta
+from config import SECURITY_APPNAME
+import requests
+
+
+url = 'https://svcs.ebay.com/services/search/FindingService/v1'
+base_params = {
+    "SECURITY-APPNAME": SECURITY_APPNAME,
+    'RESPONSE-DATA-FORMAT': 'JSON',
+    'GLOBAL-ID': 'EBAY-DE',
+    'SERVICE-VERSION': '1.0.0',
+    'OPERATION-NAME': 'findItemsByProduct',
+    'productId.@type': "ISBN",
+    'productId': '0300188374',
+    'paginationInput.pageNumber': 1,
+}
+
+
+class Offer:
+    def __init__(self, data):
+        self.price = data["sellingStatus"]["convertedCurrentPrice"][0]["__value__"]
+        assert data["sellingStatus"]["convertedCurrentPrice"][0]["@currencyId"] == "EUR"
+        self.listing_types = [info["listingType"][0] for info in data["listingInfo"]]
+
+        # we assume all listing types have same date for now:
+        end_date = data["listingInfo"][0]["listingType"][0]
+        self.end_date = datetime.strptime(end_date, "%Y-%m-%dT%H:%M:%S.%fZ")
+
+        self.url = data['viewItemURL']
+        self.title = data["title"]
+
+    @property
+    def is_auction(self):
+        return "Auction" in self.listing_types
+
+
+def _collect_all_products(isbn):
+    params = {"productId": isbn}
+    params.update(base_params)
+    response_data = requests.get(url, params).json()
+    results = response_data["findItemsByProductResponse"][0]["searchResult"][0]["item"]
+    while response_data["pageNumber"] < response_data["totalPages"]:
+        params['paginationInput.pageNumber'] += 1
+        response_data = requests.get(url, params).json()
+        results += response_data["findItemsByProductResponse"][0]["searchResult"][0]["item"]
+    return [Offer(data) for data in results]
+
+
+def find_offers(isbn, price):
+    offers = _collect_all_products(isbn)
+    offers = [offer for offer in offers if offer.price > price]
+
+    # if there are auctions, only include them if there is one day left till they end
+    offers = [
+        offer for offer in offers
+        if not offer.is_auction and (offer.end_date - datetime.now()) > timedelta(days=2)
+    ]
+    return offers
+
+
+
---
+++ main.py
@@ -0,0 +1,17 @@
+import json
+import time
+
+from ebay import find_offers
+
+
+while True:
+    with open("books.json") as file:
+        books_input = json.loads(file.read())
+    offers = [find_offers(b["isbn"], b["price"]) for b in books_input]
+
+    if offers:
+        body = "\n".join("\n".join((offer.title, offer.url)) for offer in offers)
+    time.sleep(60 * 5)
+
+
+
